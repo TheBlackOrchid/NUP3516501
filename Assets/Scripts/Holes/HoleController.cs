31a99a29cd56ef9a84dbdd53210b15a0d71dd66c
Blind56 <petrinko6@gmail.com>
2016-03-13 01:42:47 +0200

    кароч дирки дан
    ще треба стінки, які би заповняли собою пространство між дирками
    РІДМІ:
    -шоб юзати дирки не тільки зліва а і справа, треба повернути їх на 180
    градусів по Z
    постараюсь придумати шось на цей рахунок полегше

--- Assets/Scripts/Holes/HoleController.cs
+++ Assets/Scripts/Holes/HoleController.cs
@@ -3,37 +3,43 @@ using System.Collections;
 
 public class HoleController : MonoBehaviour {
 
+	public const float PosToScale = 1.561754f;
+
 	// public variables
 	public Transform target;
+	public Transform sideUp;
 	public Transform sideDown;
 	public float holeSize = 1f; // the thinest distance between hole sides;
+	public float wallWidth = 0.6f; // aka x
 
 	// public properties
 	public Vector3 targetPosition { get; private set; } // ...with our offset;
 	public float sideSeparation { get; private set; } // vertical distance between two sides
 
 	// private variables
-	private Transform myTransform;
-	private Transform[] sides;
-	private Transform[] fillers;
 	private Vector3 targetOffset; // basically a half of target's height
-
+	private float angle; //angle of side down
 
 	// Use this for initialization
 	void Start () 
 	{
-		myTransform = transform;
 		targetOffset = new Vector3 (0, target.gameObject.GetComponent<SpriteRenderer> ().bounds.extents.y);
 		targetPosition = target.position + targetOffset;
 	}
 
 	void Update () 
 	{
+		angle = Angle(sideDown.position - targetPosition);
 		AdjustHoleSize ();
 	}
 
 	void AdjustHoleSize() // must be used in Update
 	{
-		sideSeparation = holeSize / Mathf.Abs (Mathf.Cos (sideDown.localRotation.eulerAngles.z * Mathf.Deg2Rad)); // don't ask why
+		sideSeparation = (holeSize / Mathf.Abs (Mathf.Cos (angle * Mathf.Deg2Rad))) + Mathf.Abs (sideUp.localScale.y / PosToScale); // don't ask why (better draw two right triangles)
+	}	
+
+	public static float Angle(Vector3 vector) // parameter vector is a position vector
+	{
+		return Mathf.Atan(vector.y / vector.x) * Mathf.Rad2Deg; // an atangent of sine over cosine of an angle in radians converted to degrees
 	}
 }
---
+++ Assets/Scripts/Holes/SideController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideController : MonoBehaviour {
+
+	private HoleController hC;
+	private Transform myTransform;
+	private Vector3 ScaleVector;
+	private float x; // side up x position
+	private float y; // see my drawings
+	private float angle; // angle of slope
+
+	private int xDirInt; // xDir in use
+	private int xDirIntReverse; // -original
+	private int xDirIntOriginal; // initial xDir
+
+	private int yDirInt; // same thing as with xDir
+	private int yDirIntReverse;
+	private int yDirIntOriginal;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+		hC = myTransform.parent.GetComponent<HoleController> ();
+
+		xDirIntOriginal = (int)Mathf.Sign(myTransform.localScale.x);
+		xDirIntReverse = -xDirIntOriginal;
+
+		yDirIntReverse = (int)Mathf.Sign(myTransform.localScale.y);
+		yDirIntOriginal = -yDirIntReverse;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AdjustSize ();
+		AdjustPosition ();
+	}
+
+	void AdjustSize () // controlls object scale to create the slope that is needed
+	{
+		angle = HoleController.Angle(myTransform.position - hC.targetPosition);
+		y = hC.wallWidth * Mathf.Tan(angle * Mathf.Deg2Rad); // such trigonometry... to understand draw a right triangle
+
+		if (angle > 0) // angle is grater than zero when the side is under the target
+		{
+			xDirInt = xDirIntReverse; // flipping dirs
+			yDirInt = yDirIntReverse;
+		} 
+		else // ...and lesss than zero when it is below
+		{
+			xDirInt = xDirIntOriginal; // flipping dirs
+			yDirInt = yDirIntOriginal;
+		}
+
+		ScaleVector = new Vector3(hC.wallWidth * HoleController.PosToScale * xDirInt, y * HoleController.PosToScale * yDirInt, 1); //constructing a scale vector
+		myTransform.localScale = ScaleVector; // applying the scale
+	}
+
+	void AdjustPosition() 
+	{
+		if (xDirInt < 0) // in other words: if it is side down, then set its position x value to be -wall width
+		{ 
+			x = hC.wallWidth * xDirInt; // this is needed because of pivot being on the side of a sprite
+			// we use xDir because when the side is under the target we need to flip it as well
+		} 
+		else
+		{ // if it's side up - do nothing
+			x = 0;
+		}
+		myTransform.localPosition = new Vector3 (x, hC.sideSeparation / 2 * yDirIntOriginal); // applying the transformations
+	}
+}
