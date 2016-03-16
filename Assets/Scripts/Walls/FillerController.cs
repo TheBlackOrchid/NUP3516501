fe3abef1019ce35c9d914b24dafbfa7607a9286e
Blind56 <petrinko6@gmail.com>
2016-03-16 23:32:46 +0200

    стіни дан
    осталось лиш двигати дирки

---
+++ Assets/Scripts/Walls/FillerController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Position { Up, Center, Down}
+
+public class FillerController : MonoBehaviour {
+	
+	public WallController wallController;
+	public Transform upperHole; // can be null
+	public Transform lowerHole; // can be null
+	public Position position; // enum
+	public float offset = 0.001f; // to avoid holes between sprites
+
+	private Transform myTransform;
+	private HoleController uHHC; // upper hole HoleController
+	private HoleController lHHC; // lower hole HoleController
+	private HeightRange ownSegment; // height range
+	private float wallWidth;
+	private float wallVerticalExtent; // the half of the wall height
+	private int xDir; // to determine if the filler is on the right
+
+	// previous
+	private HeightRange prevOwnSegment;
+	private float prevWallWidth;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+		wallVerticalExtent = wallController.wallHeight / 2;
+
+		if (upperHole != null) {
+			uHHC = upperHole.GetComponent<HoleController> ();
+		}
+		if (lowerHole != null) {
+			lHHC = lowerHole.GetComponent<HoleController> ();
+		}
+
+		xDir = (int)-Mathf.Sign (myTransform.position.x);
+
+		wallWidth = wallController.wallWidth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateBoundaries ();
+		AdjustSize ();
+		UpdatePrevious ();
+	}
+
+	void UpdateBoundaries () // need optimizations
+	{
+		switch (position) { // building height ranges for all positions
+		case Position.Up:
+			ownSegment = new HeightRange (myTransform.parent.position.y + wallVerticalExtent, lHHC.holeExtent.up - offset);
+			break;
+		case Position.Center:
+			ownSegment = new HeightRange (uHHC.holeExtent.down + offset, lHHC.holeExtent.up - offset);
+			break;
+		case Position.Down:
+			ownSegment = new HeightRange (uHHC.holeExtent.down + offset, myTransform.parent.position.y - wallVerticalExtent);
+			break;
+		}
+	}
+
+	void AdjustSize()
+	{
+		if (ownSegment.up != prevOwnSegment.up || ownSegment.down != prevOwnSegment.down || wallWidth != prevWallWidth) { // if something has changed
+			myTransform.localPosition = Vector3.up * ownSegment.center; // then put yourself in center of you segment
+			myTransform.localScale = new Vector3 (wallWidth * HoleController.PosToScale * xDir, ownSegment.height * HoleController.PosToScale, 1); // and scale appropriately
+		}
+	}
+
+	void UpdatePrevious () // update previous
+	{
+		prevWallWidth = wallWidth;
+		prevOwnSegment = ownSegment;
+	}
+}
--- Assets/Scripts/Walls/WallController.cs
+++ Assets/Scripts/Walls/WallController.cs
@@ -24,13 +24,14 @@ public class WallController : MonoBehaviour {
 	public Transform target;
 	[Space(10)]
 	public float wallWidth = 0.6f; // actual width of the wall
-	public float wallHeight = 10f; // actual height of the wall
-	public float wallActiveHeight = 7f; // the height that is available for the holes
+	public float wallHeight = 15f; // actual height of the wall
+	public float wallActiveHeight = 10f; // the height that is available for the holes
 	[Space(10)]
 	public int holeCount = 2;
 
 	private Transform myTransform;
 	private Transform[] holes;
+	private Transform[] fillers;
 	private GameObject currSegment;
 	private HeightRange activeRange;
 	private HeightRange[] segments;
@@ -39,15 +40,30 @@ public class WallController : MonoBehaviour {
 	void Start ()
 	{
 		myTransform = transform;
-		activeRange = new HeightRange (wallHeight / 2, (wallHeight / 2) - wallActiveHeight);
+		activeRange = new HeightRange (wallHeight / 2, (wallHeight / 2) - wallActiveHeight); // the range representation of activeHeight
+
 		CreateWall ();
 		DevideIntoSegments ();
 		AdjustHolePosition ();
 	}
 
+	void Update()
+	{
+		// test
+		if (Debug.isDebugBuild) { 
+			Debug.DrawRay (new Vector3 (myTransform.position.x, segments [0].up), Vector3.down * segments [0].height, Color.red);
+			Debug.DrawRay (new Vector3 (myTransform.position.x, segments [1].up), Vector3.down * segments [1].height, Color.green);
+			HoleController hC = holes [0].GetComponent<HoleController> ();
+			Debug.DrawRay (new Vector3 (myTransform.position.x, hC.holeExtent.up), Vector3.down * hC.holeExtent.height, Color.cyan);
+			hC = holes [1].GetComponent<HoleController> ();
+			Debug.DrawRay (new Vector3 (myTransform.position.x, hC.holeExtent.up), Vector3.down * hC.holeExtent.height, Color.magenta);
+		}
+	}
+
 	void CreateWall()
 	{
 		holes = new Transform[holeCount];
+		fillers = new Transform[holeCount + 1]; // there are always one more filler than the holes
 
 		for (int i = 0; i < holeCount; i++)  // creating holes
 		{
@@ -59,7 +75,23 @@ public class WallController : MonoBehaviour {
 		for (int i = 0; i <= holeCount; i++)  // creating fillers
 		{
 			currSegment = (GameObject)Instantiate (fillerPrefab, myTransform.position, myTransform.rotation);
+			FillerController fC = currSegment.GetComponent<FillerController> ();
+			fC.wallController = this;
+
+			if (i > 0 && i < holeCount) { // if it's not the first or the last
+				fC.position = Position.Center; // then set its position (enum) to center
+				fC.upperHole = holes [i - 1]; // set the upper
+				fC.lowerHole = holes [i]; // and lower holes
+			} else if (i == holeCount) { // if it's the last 
+				fC.position = Position.Down; // then set its position (enum) to down
+				fC.upperHole = holes [i - 1]; // and set only the upper hole
+			} else { // here the opposite thing
+				fC.position = Position.Up;
+				fC.lowerHole = holes [0];
+			}
+
 			currSegment.transform.parent = myTransform;
+			fillers [i] = currSegment.transform;
 		}
 	}
 
@@ -82,7 +114,7 @@ public class WallController : MonoBehaviour {
 	{
 		for (int i = 0; i < holeCount; i++) 
 		{
-			holes [i].localPosition = Vector3.up * segments [i].center;
+			holes [i].localPosition = Vector3.up * segments [i].center; // positioning the holes in center of their segments
 		}
 	}
 }
